4d3000870bc536bc7a5953d2dd42a895def9c477
Kåre von Geijer <kare.kvg@gmail.com>
2024-05-10 11:48:09 +0200

    Fix and refactor wrapping math in realloc-queue

--- src/queue_traits.rs
+++ src/queue_traits.rs
@@ -1,15 +1,20 @@
 /// A trait to simplify generalizing over unbounded fifo queues
 pub trait FifoQueue<T> {
-    fn enqueue(&mut self, item: T);
-    fn dequeue(&mut self) -> Option<T>;
-    fn len(&self) -> usize;
-    fn peek(&self) -> Option<&T>;
     fn new() -> Self;
     fn with_capacity(capacity: usize) -> Self;
+    fn enqueue(&mut self, item: T);
+    fn dequeue(&mut self) -> Option<T>;
+    fn peek(&self) -> Option<&T>;
+    fn len(&self) -> usize;
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
 }
 
 /// A trait to simplify generalizing over unbounded double-ended queues
 pub trait Deque<T> {
+    fn new() -> Self;
+    fn with_capacity(capacity: usize) -> Self;
     fn enqueue_first(&mut self, item: T);
     fn enqueue_last(&mut self, item: T);
     fn dequeue_first(&mut self) -> Option<T>;
@@ -17,6 +22,7 @@ pub trait Deque<T> {
     fn peek_first(&self) -> Option<&T>;
     fn peek_last(&self) -> Option<&T>;
     fn len(&self) -> usize;
-    fn new() -> Self;
-    fn with_capacity(capacity: usize) -> Self;
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
 }
--- src/reallocating_queues/internal_queue.rs
+++ src/reallocating_queues/internal_queue.rs
@@ -32,45 +32,40 @@ impl<T: Sized + Clone> ReallocatingQueue<T> {
     /// Enqueue a new item at the front of the queue
     pub fn enqueue_first(&mut self, item: T) {
         // The vector is full, so we have to re-allocate it to a larger one
-        if self.size == self.buffer.capacity() {
+        if self.size == self.capacity() {
             self.reallocate(self.size * 2);
         }
 
-        if self.head == 0 && self.buffer.len() < self.buffer.capacity() {
+        if self.head == 0 && self.buffer.len() < self.capacity() {
             // We still have not filled up the vector, which we need to do to put the item at the end
-            while self.buffer.capacity() > self.buffer.len() {
+            while self.capacity() > self.buffer.len() {
                 // TODO: By using unsafe, we could initialize the whole vector in the start, avoiding the distinction between push and insert as well
                 self.buffer.push(item.clone())
             }
-
-            self.head = self.buffer.len() - 1;
-            self.size += 1;
         } else {
             // We don't have to worry about pushing, as the location is already filled
-            self.head = (self.head - 1) % self.buffer.capacity();
             self.buffer[self.head] = item;
-            self.size += 1;
         }
+        self.head = self.dec_head();
+        self.size += 1;
     }
 
     /// Enqueue a new item at the back of the queue
     pub fn enqueue_last(&mut self, item: T) {
         // The vector is full, so we have to re-allocate it to a larger one
-        if self.tail == self.head && self.size > 0 {
+        if self.size == self.capacity() {
             self.reallocate(self.size * 2);
         }
 
-        if self.buffer.len() < self.buffer.capacity() {
+        if self.buffer.len() < self.capacity() {
             // We still have not filled up the vector, so use push
             self.buffer.push(item);
-            self.tail = (self.tail + 1) % self.buffer.capacity();
-            self.size += 1;
         } else {
             // We have filled up the vector, so now we wrap
             self.buffer[self.tail] = item;
-            self.tail = (self.tail + 1) % self.buffer.capacity();
-            self.size += 1;
         }
+        self.tail = self.inc_tail();
+        self.size += 1;
     }
 
     /// Reallocates the underlying vector to a different size, must be larger than the queue size
@@ -82,21 +77,20 @@ impl<T: Sized + Clone> ReallocatingQueue<T> {
         }
 
         // Replace the old vector with a new one
-        let mut old_buffer = std::mem::replace(&mut self.buffer, Vec::with_capacity(capacity));
-        let old_capacity = old_buffer.capacity();
+        let mut new_buffer = Vec::with_capacity(capacity);
 
         // Add the items in their correct order from the old buffer to the new
-        if self.head + self.size > old_capacity {
+        if self.head + self.size > self.capacity() {
             // The items wrap around the end of the buffer
-            self.buffer
-                .extend(old_buffer.drain(self.head..old_capacity));
-            self.buffer.extend(old_buffer.drain(0..self.tail));
+            new_buffer.extend(self.buffer.drain(self.head..self.capacity()));
+            new_buffer.extend(self.buffer.drain(0..self.tail));
         } else {
             // The items are contiguous without any wrapping
-            self.buffer.extend(old_buffer.drain(self.head..self.tail));
+            new_buffer.extend(self.buffer.drain(self.head..=self.dec_tail()));
         }
 
         // Make sure to update the head and tail
+        self.buffer = new_buffer;
         self.head = 0;
         self.tail = self.size;
     }
@@ -105,7 +99,7 @@ impl<T: Sized + Clone> ReallocatingQueue<T> {
     pub fn dequeue_first(&mut self) -> Option<T> {
         if self.size > 0 {
             let item = self.buffer[self.head].clone();
-            self.head = (self.head + 1) % self.buffer.capacity();
+            self.head = self.inc_head();
             self.size -= 1;
             Some(item)
         } else {
@@ -116,7 +110,7 @@ impl<T: Sized + Clone> ReallocatingQueue<T> {
     /// Dequeue the last item in the queue
     pub fn dequeue_last(&mut self) -> Option<T> {
         if self.size > 0 {
-            self.tail = (self.tail - 1) % self.buffer.capacity();
+            self.tail = self.dec_tail();
             let item = self.buffer[self.head].clone();
             self.size -= 1;
             Some(item)
@@ -137,7 +131,7 @@ impl<T: Sized + Clone> ReallocatingQueue<T> {
     /// Peek at the last item in the queue
     pub fn peek_last(&self) -> Option<&T> {
         if self.size > 0 {
-            let index = (self.tail - 1) % self.buffer.capacity();
+            let index = self.dec_tail();
             Some(&self.buffer[index])
         } else {
             None
@@ -147,5 +141,56 @@ impl<T: Sized + Clone> ReallocatingQueue<T> {
     /// Returns the number of items currently in the queue
     pub fn len(&self) -> usize {
         self.size
+    }
+
+    /// Gets the capacity of the current buffer
+    fn capacity(&self) -> usize {
+        self.buffer.capacity()
+    }
+
+    /// Returns tail incremented by 1, modulo self.capacity()
+    fn inc_tail(&self) -> usize {
+        wrapping_inc(self.tail, self.capacity())
+    }
+
+    /// Returns tail decremented by 1, modulo self.capacity()
+    fn dec_tail(&self) -> usize {
+        wrapping_dec(self.tail, self.capacity())
+    }
+
+    /// Returns head incremented by 1, modulo self.capacity()
+    fn inc_head(&self) -> usize {
+        wrapping_inc(self.head, self.capacity())
+    }
+
+    /// Returns head decremented by 1, modulo self.capacity()
+    fn dec_head(&self) -> usize {
+        wrapping_dec(self.head, self.capacity())
+    }
+}
+
+/// Subtracts base by 1 in modular arithmetic with wrap.
+fn wrapping_dec(mut base: usize, wrap: usize) -> usize {
+    if base >= wrap {
+        base = base % wrap;
+    }
+
+    if base != 0 {
+        base - 1
+    } else {
+        wrap - 1
+    }
+}
+
+/// Increments base by 1 in modular arithmetic with wrap.
+fn wrapping_inc(mut base: usize, wrap: usize) -> usize {
+    if base >= wrap {
+        base = base % wrap;
+    }
+
+    if base + 1 != wrap {
+        base + 1
+    } else {
+        0
     }
 }
